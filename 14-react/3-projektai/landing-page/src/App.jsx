af263dbf7e37beea4a3c6affd1e51637826bf944
viklopova <klopova.viktorija@gmail.com>
2025-05-08 18:09:43 +0300

    updated

--- 14-react/3-projektai/landing-page/src/App.jsx
+++ 14-react/3-projektai/landing-page/src/App.jsx
@@ -1,95 +1,92 @@
-import { useEffect, useState } from 'react';
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import JsonData from './data/data.json';
-import Navigation from './components/navigation/Navigation';
-import Header from './pages/header/Header';
-import Features from './pages/features/Features';
-import About from './pages/aboutUs/About';
-import OurServices from './pages/OurServices/OurServices';
-import Gallery from './pages/gallery/Gallery';
-import Testimonials from './pages/feedback/Testimonials';
-import Team from './pages/team/Team';
-import Contacts from './pages/contacts/Contacts';
-
+import { useEffect, useState } from "react";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import JsonData from "./data/data.json";
+import Navigation from "./components/navigation/Navigation";
+import Header from "./pages/header/Header";
+import Features from "./pages/features/Features";
+import About from "./pages/aboutUs/About";
+import OurServices from "./pages/OurServices/OurServices";
+import Gallery from "./pages/gallery/Gallery";
+import Testimonials from "./pages/feedback/Testimonials";
+import Team from "./pages/team/Team";
+import Contacts from "./pages/contacts/Contacts";
 
 function App() {
   const [landingPageData, setLandingPageData] = useState(null);
 
   useEffect(() => {
     setLandingPageData(JsonData);
-  }, [])
+  }, []);
 
   return (
     <>
-      {
-        landingPageData && (
-          <>
-            <Navigation 
-              title={landingPageData.navigation.title}
-              navItems={landingPageData.navigation.navigationItems}
-            />
+      {landingPageData && (
+        <>
+          <Navigation
+            title={landingPageData.navigation.title}
+            navItems={landingPageData.navigation.navigationItems}
+          />
 
-            <div id="header">
-              <Header
-                title={landingPageData.header.title}
-                subtitle={landingPageData.header.subtitle}
-                button={landingPageData.header.button}
-              />
-            </div>
-            <div id="features">
-              <Features
-                title={landingPageData.features.title}
-                featuresItems={landingPageData.features.featuresItems}
-              />
-            </div>
-            <div id="about-us">
-              <About
-                title={landingPageData.about.title}
-                description={landingPageData.about.description}
-                whyChooseUs={landingPageData.about.whyChooseUs}
-                whyChooseList={landingPageData.about.whyChooseList}
-              />
-            </div>
-            <div id="services">
-              <OurServices
-                title={landingPageData.services.title}
-                subtitle={landingPageData.services.subtitle}
-                servicesItems={landingPageData.services.servicesItems}
-              />
-            </div>
-            <div id="gallery">
-              <Gallery
-                title={landingPageData.gallery.title}
-                description={landingPageData.gallery.description}
-                galleryItems={landingPageData.gallery.galleryItems}
-              />
-            </div>
-            <div id="testimonials">
-              <Testimonials
-                title={landingPageData.testimonials.title}
-                feedbackItems={landingPageData.testimonials.feedbackItems}
-              />
-            </div>
-            <div id="team">
-              <Team
-                title={landingPageData.team.title}
-                teamItems={landingPageData.team.teamItems}
-              />
-            </div>
-            <div id="contacts">
-              <Contacts
-                title={landingPageData.contacts.title}
-                subtitle={landingPageData.contacts.subtitle}
-                contactInfo={landingPageData.contacts.contactInfo}
-                contactInfoItems={landingPageData.contacts.contactInfoItems}
-              />
-            </div>
-          </>
-        )
-      }
+          <div id="header">
+            <Header
+              title={landingPageData.header.title}
+              subtitle={landingPageData.header.subtitle}
+              button={landingPageData.header.button}
+            />
+          </div>
+          <div id="features">
+            <Features
+              title={landingPageData.features.title}
+              featuresItems={landingPageData.features.featuresItems}
+            />
+          </div>
+          <div id="about">
+            <About
+              title={landingPageData.about.title}
+              description={landingPageData.about.description}
+              whyChooseUs={landingPageData.about.whyChooseUs}
+              whyChooseList={landingPageData.about.whyChooseList}
+            />
+          </div>
+          <div id="services">
+            <OurServices
+              title={landingPageData.services.title}
+              subtitle={landingPageData.services.subtitle}
+              servicesItems={landingPageData.services.servicesItems}
+            />
+          </div>
+          <div id="gallery">
+            <Gallery
+              title={landingPageData.gallery.title}
+              description={landingPageData.gallery.description}
+              galleryItems={landingPageData.gallery.galleryItems}
+            />
+          </div>
+          <div id="testimonials">
+            <Testimonials
+              title={landingPageData.testimonials.title}
+              feedbackItems={landingPageData.testimonials.feedbackItems}
+            />
+          </div>
+          <div id="team">
+            <Team
+              title={landingPageData.team.title}
+              teamItems={landingPageData.team.teamItems}
+            />
+          </div>
+          <div id="contacts">
+            <Contacts
+              title={landingPageData.contacts.title}
+              subtitle={landingPageData.contacts.subtitle}
+              contactInfo={landingPageData.contacts.contactInfo}
+              contactInfoItems={landingPageData.contacts.contactInfoItems}
+            />
+          </div>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 14-react/3-projektai/landing-page/src/pages/contacts/Contacts.jsx
+++ 14-react/3-projektai/landing-page/src/pages/contacts/Contacts.jsx
@@ -1,52 +1,63 @@
-import React from 'react'
-import { Container, Row, Col } from "react-bootstrap"
-import './Contacts.scss'
-import data from '../../data/data.json'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./Contacts.scss";
+import data from "../../data/data.json";
 import * as icons from "react-icons/ci";
 
-
 const Contacts = () => {
-    const { title, subtitle, contactInfo, contactInfoItems } = data.contacts;
-    return (
-        <section className="contacts">
-            <Container>
-                <Row>
-                    <Col lg={8} className="contact-form">
-                        <h2>{title}</h2>
-                        <span className="section-underline"></span>
-                        <p>{subtitle}</p>
-                        <form>
-                            <div className="form-group mb-4">
-                                <input type="text" className="form-control" placeholder="Name" />
-                            </div>
-                            <div className="form-group mb-4">
-                                <input type="email" className="form-control" placeholder="Email" />
-                            </div>
-                            <div className="form-group mb-4">
-                                <textarea className="form-control" rows="4" placeholder="Message"></textarea>
-                            </div>
-                            <button type="submit" className="btn btn-primary">Wingardium Leviosa</button>
-                        </form>
-                    </Col>
-                    <Col lg={4} className="contact-info">
-                        <h3>{contactInfo}</h3>
-                        <div className="info-items">
-                            {contactInfoItems.map((item, index) => (
-                                <div key={index} className="info-item">
-                                    <div className="info-icon">
-                                        {icons[item.icon]}
-                                    </div>
-                                    <div className="info-content">
-                                        <h4>{item.title}</h4>
-                                        <p>{item.text}</p>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    )
-}
-export default Contacts
+  const { title, subtitle, contactInfo, contactInfoItems } = data.contacts;
+  return (
+    <section className="contacts">
+      <Container>
+        <Row>
+          <Col lg={8} className="contact-form">
+            <h2>{title}</h2>
+            <span className="section-underline"></span>
+            <p>{subtitle}</p>
+            <form>
+              <div className="form-group mb-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                />
+              </div>
+              <div className="form-group mb-4">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="form-group mb-4">
+                <textarea
+                  className="form-control"
+                  rows="4"
+                  placeholder="Message"
+                ></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Wingardium Leviosa
+              </button>
+            </form>
+          </Col>
+          <Col lg={4} className="contact-info">
+            <h3>{contactInfo}</h3>
+            <div className="info-items">
+              {contactInfoItems.map((item, index) => (
+                <div key={index} className="info-item">
+                  <div className="info-icon">{icons[item.icon]()}</div>
+                  <div className="info-content">
+                    <h4>{item.title}</h4>
+                    <p>{item.text}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+export default Contacts;
